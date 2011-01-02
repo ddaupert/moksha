2ca052b8f8d9fe4354a3af6c6b43e2c496f35e20
Dennis Daupert <ddaupert@sbcglobal.net>
2011-01-02 11:22:59 -0500

    Deleted Clear.pm, modified story_type sql list

--- db/70_insert_story_types.sql
+++ db/70_insert_story_types.sql
@@ -1,5 +1,2 @@
-INSERT INTO story_type VALUES(1,'IT',1);
-INSERT INTO story_type VALUES(2,'Computer',1);
-INSERT INTO story_type VALUES(3,'Feeling',1);
-INSERT INTO story_type VALUES(4,'Sports',1);
-INSERT INTO story_type VALUES(5,'Healthy',1);
+INSERT INTO story_type VALUES(1,'General',1);
+INSERT INTO story_type VALUES(2,'Main',1);
